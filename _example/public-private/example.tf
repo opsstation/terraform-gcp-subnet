6dce9c60d43796e34647f2f3448340eb03a80350
abhisharma24 <theabhaysharma24@gmail.com>
2025-10-27 12:30:34 +0530

    update modules

--- _example/public-private/example.tf
+++ _example/public-private/example.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "opsstation"
+  project = "opsstation-474608"
   region  = "us-west1"
   zone    = "asia-northeast1-a"
 }
@@ -15,10 +15,12 @@ module "vpc" {
 
 #===============================(subnet)=================================
 module "subnet" {
-  source        = "../.."
-  name          = ["subnet-a"]
-  environment   = "test"
-  region        = "asia-northeast1"
-  network       = module.vpc.vpc_id
-  ip_cidr_range = ["10.10.1.0/24"]
+  source           = "../.."
+  name             = ["subnet-public-1", "subnet-public-2", "subnet-public-3", "subnet-private-1", "subnet-private-2", "subnet-private-3"]
+  environment      = "test"
+  region           = "asia-northeast1"
+  subnet_type      = ["public", "public", "public", "private", "private", "private"]
+  network          = module.vpc.vpc_id
+  ip_cidr_range    = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+  multiple_subnets = true
 }
--- _example/single/example.tf
+++ _example/single/example.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "opsstation"
+  project = "opsstation-474608"
   region  = "us-west1"
   zone    = "asia-northeast1-a"
 }
@@ -7,19 +7,19 @@ provider "google" {
 module "vpc" {
   source                                    = "git::git@github.com:opsstation/terraform-gcp-vpc.git?ref=feat/release-1"
   name                                      = "dev"
-  environment                               = "test"
+  environment                               = "nonprod"
   routing_mode                              = "REGIONAL"
   mtu                                       = 1500
   network_firewall_policy_enforcement_order = "BEFORE_CLASSIC_FIREWALL"
 }
 
 #===============================(subnet)=================================
+
 module "subnet" {
-  source           = "../.."
-  name             = ["subnet-a", "subnet-b"]
-  environment      = "test"
-  region           = "asia-northeast1"
-  network          = module.vpc.vpc_id
-  ip_cidr_range    = ["10.10.1.0/24", "10.10.5.0/24"]
-  multiple_subnets = true
+  source        = "../.."
+  name          = ["subnet-a"]
+  environment   = "nonprod"
+  region        = "asia-northeast1"
+  network       = module.vpc.vpc_id
+  ip_cidr_range = ["10.10.1.0/24"]
 }
